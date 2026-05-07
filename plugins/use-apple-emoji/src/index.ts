29b3fc20fc10ba4d626d198716eebc498c826a0f
ᥱƖƖყ ★ <giihlustosa@gmail.com>
2026-05-06 23:56:59 -0300

    index.ts

--- plugins/use-apple-emoji/src/index.ts
+++ plugins/use-apple-emoji/src/index.ts
@@ -1,40 +1,39 @@
-import { patcher } from "@vendetta";
 import { findByProps } from "@vendetta/metro";
 import { React, ReactNative } from "@vendetta/metro/common";
 
-const unpatchers: (() => void)[] = [];
 let logged = false;
+let origEmoji: any = null;
+let emojiMod: any = null;
 
 function init() {
-    const emojiMod = findByProps("Emoji", "asUnicodeEmoji");
-    if (!emojiMod) { alert("emojiMod null"); return; }
+    emojiMod = findByProps("Emoji", "asUnicodeEmoji");
+    if (!emojiMod?.Emoji) { alert("emojiMod null"); return; }
 
-    unpatchers.push(
-        patcher.instead(emojiMod, "Emoji", function(args: any[], orig: Function) {
-            const props = args[0];
-            if (!logged) {
-                logged = true;
-                const surrogate =
-                    props?.emoji?.surrogates ??
-                    props?.node?.surrogate ??
-                    props?.surrogates ??
-                    "N/A";
-                const Image = ReactNative?.Image;
-                alert(
-                    "Emoji chamado!\n" +
-                    "props keys: " + Object.keys(props ?? {}).join(", ") + "\n" +
-                    "surrogate: " + surrogate + "\n" +
-                    "ReactNative.Image: " + (Image ? "OK" : "NULL")
-                );
-            }
-            return orig.apply(this, args);
-        })
-    );
+    origEmoji = emojiMod.Emoji;
+
+    emojiMod.Emoji = function(props: any) {
+        if (!logged) {
+            logged = true;
+            const surrogate =
+                props?.emoji?.surrogates ??
+                props?.node?.surrogate ??
+                props?.surrogates ??
+                "N/A";
+            alert(
+                "Emoji chamado!\n" +
+                "props keys: " + Object.keys(props ?? {}).join(", ") + "\n" +
+                "surrogate: " + surrogate + "\n" +
+                "Image: " + (ReactNative?.Image ? "OK" : "NULL")
+            );
+        }
+        return origEmoji(props);
+    };
 }
 
 init();
 
 export const onUnload = () => {
-    for (const up of unpatchers) up();
-    unpatchers.length = 0;
+    if (emojiMod && origEmoji) {
+        emojiMod.Emoji = origEmoji;
+    }
 };
