5eede487805905d89a52b8031eab591747dc3494
ellyzzinha <ellyzzinha@users.noreply.github.com>
2026-05-07 03:59:24 +0000

    deploy: 8537071ff5be57011788c5a064f47186f0e750f3

--- use-apple-emoji/index.js
+++ use-apple-emoji/index.js
@@ -1,5 +1,6 @@
-(function(r,l){"use strict";let t=null,e=null,s=!1;function a(){if(t=l.findByProps("updateRows","clearRows"),!t){alert("rowMod null");return}e=t.updateRows,t.updateRows=function(...o){if(!s){s=!0;const n=o[0];alert("args.length: "+o.length+`
-rows type: `+typeof n+`
-isArray: `+Array.isArray(n)+`
-rows length: `+(Array.isArray(n)?n.length:"N/A")+`
-keys: `+Object.keys(n??{}).slice(0,10).join(", "))}return e.apply(this,o)}}a();const u=function(){t&&e&&(t.updateRows=e)};return r.onUnload=u,r})({},vendetta.metro);
+(function(a,y){"use strict";let t=null,n=null,s=!1;function d(){if(t=y.findByProps("updateRows","clearRows"),!t){alert("rowMod null");return}n=t.updateRows,t.updateRows=function(...l){if(!s){s=!0;const e=l[1],r=Array.isArray(e),o=r?e[0]:null,c=Object.keys(o??{}).join(", "),u=o?.message?.content,i=Array.isArray(u)?u[0]:null;alert("args[1] isArray: "+r+`
+length: `+(r?e.length:"N/A")+`
+first keys: `+c+`
+first.type: `+o?.type+`
+node.type: `+i?.type+`
+node.surrogate: `+i?.surrogate)}return n.apply(this,l)}}d();const p=function(){t&&n&&(t.updateRows=n)};return a.onUnload=p,a})({},vendetta.metro);
